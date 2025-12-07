aa58b423b7e71868eb0c3dce97a3b6a104ee2eee
Zyndrahr <97333494+Zyndrahr@users.noreply.github.com>
2025-12-07 09:18:20 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,16 @@
+
+
+let FirstName = 'Lakshmanakumar M'
+let CompanyName = null
+let MobileNumber = 9994825097n
+let isAutomation = true
+let hasPlaywright 
+
+console.log 
+(
+    'FirstName = ' + typeof FirstName + " " + FirstName + '\n' +
+    'CompanyName = ' + typeof CompanyName + " " +  CompanyName + '\n' +
+    'MobileNumber = ' + typeof MobileNumber  + " "+  MobileNumber + '\n' +
+    'isAutomation = '  + typeof isAutomation + " " + isAutomation + '\n' +
+    'hasPlaywright = '  + typeof hasPlaywright + " " + hasPlaywright + '\n'
+)
